b8c7aeaacef2ca7ff624443cd8184f3fcbd2fe77
rain46688 <rain46688@gmail.com>
2024-01-30 16:19:20 +0900

    address_excel

--- apiv1/models.py
+++ apiv1/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class AdmCodeList(models.Model):
+    lowestAdmCodeNm = models.CharField(max_length=255)
+    admCodeNm = models.CharField(max_length=255)
+    admCode = models.CharField(max_length=10)   
+
+class AdmSiList(models.Model):
+    lowestAdmCodeNm = models.CharField(max_length=255)
+    admCodeNm = models.CharField(max_length=255)
+    admCode = models.CharField(max_length=10)
+
+class AdmDongList(models.Model):
+    lowestAdmCodeNm = models.CharField(max_length=255)
+    admCodeNm = models.CharField(max_length=255)
+    admCode = models.CharField(max_length=10)
+
+class AdmReeList(models.Model):
+    lowestAdmCodeNm = models.CharField(max_length=255)
+    admCodeNm = models.CharField(max_length=255)
+    admCode = models.CharField(max_length=10)
--- apiv1/views.py
+++ apiv1/views.py
@@ -1,6 +1,65 @@
-from django.http import JsonResponse
 import requests
+import pandas as pd
+from django.http import JsonResponse
+from apiv1.models import AdmCodeList, AdmSiList, AdmDongList, AdmReeList
+from django.db import IntegrityError
 
+# 메인 로직
+def main(request):
+    print(" ===== main ===== ")
+
+    # 리스트 선언
+    admCodeLists= []
+    admSiList=[]
+    admDongList=[]
+    admReeList=[]
+
+    # 실행 로직
+    try:
+        print(" ===== 시도 리스트 구하기 ===== ")
+        admCodeLists = address("admCodeList", AdmCodeList)
+
+        print(" ===== 시군구 리스트 구하기 ===== ")
+        admSiList = address("admSiList", AdmSiList, admCodeLists)
+
+        print(" ===== 읍면동 리스트 구하기 ===== ")
+        admDongList = address("admDongList", AdmDongList, admSiList)
+
+        print(" ===== 리 리스트 구하기 ===== ")
+        admReeList = address("admReeList", AdmReeList, admDongList)
+
+        print(" ===== 전체 로직 완료 ===== ")
+
+        return JsonResponse({"status" : "success"}, safe=False)
+    except Exception as e:
+        print(f"e : {e}")
+        return JsonResponse({"status" : "fail"}, safe=False)
+    
+# 메인 로직 - 메인 실행안하면 디버깅 안됨
+if __name__ == "__main__":
+    main("main")
+
+# 리스트 반환 로직
+def address(name, db, plist=[]):
+    req_data = []
+    if plist == []:
+        req_data = api(name, None)
+    else:
+        for item in plist:
+            list = api(name, item.get('admCode', None))
+            print(f"name : {name} / admCode : {item.get('admCode', None)}")
+            if list:
+                req_data.extend(list)
+    print(" ===== 리스트 생성 완료 ===== ")
+    savedb(req_data, db)
+    print(" ===== 디비 추가 완료 ===== ")
+    edata = excel(db)
+    print(" ===== 엑셀 작업 완료 ===== ")
+    edata.to_excel(name + ".xlsx", index=False)
+    print(" ===== 리스트 반환 로직 완료 ===== ")
+    return req_data
+
+# api 호출 로직
 def api(name, admCode):
         api_url = "https://api.vworld.kr/ned/data/"+name
 
@@ -8,7 +67,7 @@ def api(name, admCode):
             'key': '56C7879D-F177-3925-83BE-62B81A40452E',
             "domain": "http://localhost:3000/",
             "format": "json",
-            "numOfRows": "5000",
+            "numOfRows": "1000",
             "pageNo": "1"
         }
 
@@ -19,50 +78,28 @@ def api(name, admCode):
 
         if response.status_code == 200:
             data = data = response.json()
+            # print(f"data : {data}")
             return data.get('admVOList', {}).get('admVOList', None)
         else:
-            return JsonResponse({'에러'}, status=500)
-    
-def main(request):
-    print("main")
-    admCodeList= []
-    admSiList=[]
-    admDongList=[]
-    admReeList=[]
+            print("에러")
+            return None
 
-    # # 시도 리스트 구하기
-    # print("시도 리스트 구하기")
-    # admCodeList = api("admCodeList", None)
+# pandas 저장 로직
+def savedb(data, model):
+    try:
+        if data is not None:
+            for item in data:
+                # 중복된 데이터인지 확인
+                existing_data = model.objects.filter(admCode=item.get('admCode', None)).first()
+                if existing_data is None:
+                    # 중복되지 않은 경우에만 추가
+                    print(f" ===== admCode: {item.get('admCode', None)} - 신규 데이터 추가 ===== ")
+                    model.objects.create(**item)
+    except IntegrityError as e:
+        print(f" ===== 에러 : {e} =====")
 
-    # # 시군구 리스트 구하기
-    # print("시군구 리스트 구하기")
-    # for item in admCodeList:
-    #     list= api("admSiList", item.get('admCode', None))
-    #     if list:
-    #         admSiList.extend(list)
-
-    # # 읍면동 리스트 구하기
-    # print("읍면동 리스트 구하기")
-    # for item in admSiList:
-    #     list= api("admDongList", item.get('admCode', None))
-    #     if list:
-    #         admDongList.extend(list)
-
-    # # 리 리스트 구하기
-    # print("리 리스트 구하기")
-    # for item in admDongList:
-    #     list= api("admReeList", item.get('admCode', None))
-    #     if list:
-    #         admReeList.extend(list)
-
-    # return JsonResponse(admDongList, safe=False)
-
-    list= api("admReeList", 52720400)
-    print(f"list : {list}")
-    if list:
-         admReeList.extend(list)
-
-    return JsonResponse(admReeList, safe=False)
-
-if __name__ == "__main__":
-    main("main")
+# 엑셀 저장 로직
+def excel(model):
+    queryset = model.objects.all()
+    data = pd.DataFrame.from_records(queryset.values())
+    return data
